69344daa73bd709cb9c03ba2b756a4399a2593b5
Rad <gilreathdarr@gmail.com>
2023-03-31 22:43:54 -0400

    Updates

---
+++ precache-manifest.27826f5d872bcd2a7da906c11f5f44cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "183bb7c390d347553479",
+    "url": "/smartbrain/static/css/main.3427f28e.chunk.css"
+  },
+  {
+    "revision": "183bb7c390d347553479",
+    "url": "/smartbrain/static/js/main.183bb7c3.chunk.js"
+  },
+  {
+    "revision": "3a79397ac1a1b3a66e79",
+    "url": "/smartbrain/static/js/1.3a79397a.chunk.js"
+  },
+  {
+    "revision": "a50c5a55ea5aaae381bc",
+    "url": "/smartbrain/static/css/2.266e83ea.chunk.css"
+  },
+  {
+    "revision": "a50c5a55ea5aaae381bc",
+    "url": "/smartbrain/static/js/2.a50c5a55.chunk.js"
+  },
+  {
+    "revision": "3cf93d0a58b6636de65a",
+    "url": "/smartbrain/static/js/runtime~main.3cf93d0a.js"
+  },
+  {
+    "revision": "41b48c2a31f1b23e6688c1950aa0e825",
+    "url": "/smartbrain/index.html"
+  }
+];
