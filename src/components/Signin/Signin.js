15963d201dc72ca57089c56f509bbcd95e0ea4c6
Darr <118402014+DigitalDarr@users.noreply.github.com>
2024-12-17 17:15:24 -0500

    Update Signin.js

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 
 class Signin extends React.Component {
   constructor(props) {
@@ -75,6 +75,93 @@ class Signin extends React.Component {
             <div className="lh-copy mt3">
               <p  onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
             </div>
+          </div>
+        </main>
+      </article>
+    );
+  }
+}
+
+export default Signin;*/
+
+import React from 'react';
+
+class Signin extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      signInEmail: '',
+      signInPassword: '',
+      error: ''
+    }
+  }
+
+  onEmailChange = (event) => {
+    this.setState({ signInEmail: event.target.value });
+  }
+
+  onPasswordChange = (event) => {
+    this.setState({ signInPassword: event.target.value });
+  }
+
+  onSubmitSignIn = () => {
+    const { signInEmail, signInPassword } = this.state;
+
+    // Hardcoded demo user credentials
+    const validUser = { email: 'demo@user.com', password: 'password123', id: 1, name: 'Demo User' };
+
+    // Check if entered email and password match the valid user
+    if (signInEmail === validUser.email && signInPassword === validUser.password) {
+      this.props.loadUser(validUser); // Pass user details
+      this.props.onRouteChange('home');
+    } else {
+      this.setState({ error: 'Invalid email or password' });
+    }
+  }
+
+  render() {
+    const { error } = this.state;
+    const { onRouteChange } = this.props;
+
+    return (
+      <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+        <main className="pa4 black-80">
+          <div className="measure">
+            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+              <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+              <div className="mt3">
+                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                <input
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="email"
+                  name="email-address"
+                  id="email-address"
+                  onChange={this.onEmailChange}
+                />
+              </div>
+              <div className="mv3">
+                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                <input
+                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="password"
+                  name="password"
+                  id="password"
+                  onChange={this.onPasswordChange}
+                />
+              </div>
+            </fieldset>
+            <div className="">
+              <input
+                onClick={this.onSubmitSignIn}
+                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                type="submit"
+                value="Sign in"
+              />
+            </div>
+            <div className="lh-copy mt3">
+              <p onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
+            </div>
+            {error && <div style={{ color: 'red', marginTop: '10px' }}>{error}</div>}
           </div>
         </main>
       </article>
