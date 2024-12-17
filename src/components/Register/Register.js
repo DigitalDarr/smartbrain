82ff0660375741862233c4441c839564279aae81
Darr <118402014+DigitalDarr@users.noreply.github.com>
2024-12-17 17:12:25 -0500

    Update Register.js

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 
 class Register extends React.Component {
   constructor(props) {
@@ -87,6 +87,104 @@ class Register extends React.Component {
                 value="Register"
               />
             </div>
+          </div>
+        </main>
+      </article>
+    );
+  }
+}
+
+export default Register; */
+
+import React from 'react';
+
+class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      name: '',
+      error: ''
+    }
+  }
+
+  onNameChange = (event) => {
+    this.setState({ name: event.target.value });
+  }
+
+  onEmailChange = (event) => {
+    this.setState({ email: event.target.value });
+  }
+
+  onPasswordChange = (event) => {
+    this.setState({ password: event.target.value });
+  }
+
+  onSubmitSignIn = () => {
+    const { email, password, name } = this.state;
+
+    // Hardcoded demo user credentials
+    const validUser = { email: 'demo@user.com', password: 'password123', id: 1, name: 'Demo User' };
+
+    // Check if entered email and password match the valid user
+    if (email === validUser.email && password === validUser.password) {
+      this.props.loadUser(validUser); // Pass user details
+      this.props.onRouteChange('home');
+    } else {
+      this.setState({ error: 'Invalid email or password' });
+    }
+  }
+
+  render() {
+    const { error } = this.state;
+
+    return (
+      <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+        <main className="pa4 black-80">
+          <div className="measure">
+            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+              <legend className="f1 fw6 ph0 mh0">Register</legend>
+              <div className="mt3">
+                <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
+                <input
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="text"
+                  name="name"
+                  id="name"
+                  onChange={this.onNameChange}
+                />
+              </div>
+              <div className="mt3">
+                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                <input
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="email"
+                  name="email-address"
+                  id="email-address"
+                  onChange={this.onEmailChange}
+                />
+              </div>
+              <div className="mv3">
+                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                <input
+                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="password"
+                  name="password"
+                  id="password"
+                  onChange={this.onPasswordChange}
+                />
+              </div>
+            </fieldset>
+            <div className="">
+              <input
+                onClick={this.onSubmitSignIn}
+                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                type="submit"
+                value="Register"
+              />
+            </div>
+            {error && <div style={{ color: 'red', marginTop: '10px' }}>{error}</div>}
           </div>
         </main>
       </article>
